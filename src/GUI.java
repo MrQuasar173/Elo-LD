97901ac58f17aa025f4acae406cd4f8ecc9a5b88
Battlecode2020 <zachary78922@gmail.com>
2021-09-11 12:59:00 -0600

    Added the beginning of a gui

---
+++ src/GUI.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+    public GUI(){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JButton eloButton = new JButton("Elo Sort");
+        JButton affEloButton = new JButton("Aff Elo Sort");
+        JButton negEloButton = new JButton("Neg Elo Sort");
+        JButton gamesPlayed = new JButton("Games Played Sort");
+        JButton elimRounds = new JButton("Elim Round Sort");
+        JButton winPercentage = new JButton("Win Percentage Sort");
+        JButton wins = new JButton("Wins Sort");
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(eloButton);
+        panel.add(affEloButton);
+        panel.add(negEloButton);
+        panel.add(gamesPlayed);
+        panel.add(elimRounds);
+        panel.add(winPercentage);
+        panel.add(wins);
+
+        eloButton.addActionListener(this);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Elo Scores");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args){
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
